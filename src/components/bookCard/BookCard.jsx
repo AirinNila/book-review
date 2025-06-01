eeb4c585ea318313d0e542fa27c65c3d119a1ea4
AirinNila <airinakhternila>
2025-06-01 17:51:14 +0600

    added books card

---
+++ src/components/bookCard/BookCard.jsx
@@ -0,0 +1,31 @@
+
+import { Link } from 'react-router-dom';
+
+const BookCard = ({data}) => {
+    const {bookName, author, image, category, rating} = data
+    return (
+        <div>
+            <Link to="/bookdetails">
+                <div className='flex flex-col p-4 rounded-md border-2 border-[#13131326] gap-4'>
+                    <div className='flex justify-center items-center bg-[#F3F3F3] rounded-md h-64'>
+                    <img src={image} alt="" className='w-32 shadow-xl' />
+                    </div>
+                    <div className='flex flex-col gap-3 '>
+                        <div className='flex items-center gap-3 primary-c'>
+                            <p>{data.tags[0]}</p>
+                            <p>{data.tags[1]} </p>
+                        </div>
+                        <h2 className='font-bold text-2xl'>{bookName}</h2>
+                        <p className='font-semibold'>By: {author}</p>
+                        <div className='flex items-center justify-between'>
+                            <p>{category}</p>
+                            <p>{rating}</p>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default BookCard;
--- src/components/booksCard/BooksCard.jsx
+++ src/components/booksCard/BooksCard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import BookCard from "../bookCard/bookCard";
 
 
 const BooksCard = () => {
@@ -8,14 +9,21 @@ const BooksCard = () => {
         .then(res => res.json())
         .then(data => setBooks(data))
     },[])
-    console.log(books)
+
+    const handleShowAllBtn = () => {
+        
+    }
+   
     return (
         <div className="container mx-auto mt-4">
-            <div className="flex justify-center gap-4">
+            <div className="flex flex-col justify-center items-center gap-4">
             <h2 className="playf text-3xl font-bold">Books Card</h2>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-3"> 
-
+                {
+                    books.slice(0,6).map(data => <BookCard key={data.bookId} data={data}></BookCard>)
+                }
             </div>
+            <button className="btn primary-bg p-4 text-white">Show All</button>
             </div>
         </div>
     );
